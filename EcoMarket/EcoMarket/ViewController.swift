f83e2a5e7d96172f7a728fca5f19b62a77420f62
ospanovadinara <ospan.dinara@yandex.ru>
2024-04-30 15:16:51 +0500

    удаление Main сториборда, добавление rootViewController

---
+++ EcoMarket/EcoMarket/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  EcoMarket
+//
+//  Created by Dinara on 30.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
