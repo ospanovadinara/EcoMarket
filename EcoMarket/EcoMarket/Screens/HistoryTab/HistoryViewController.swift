7de50a6f79e3c3f379adb917519e78a39376b17f
ospanovadinara <ospan.dinara@yandex.ru>
2024-06-12 16:53:32 +0500

    marks fixes

--- EcoMarket/EcoMarket/Screens/HistoryTab/HistoryViewController.swift
+++ EcoMarket/EcoMarket/Screens/HistoryTab/HistoryViewController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
+import SnapKit
 
+// MARK: - Class HistoryViewController
 final class HistoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
